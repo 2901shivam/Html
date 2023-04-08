c3c38713ed93a54b74a3ab51bd3e76cd5870a7fa
= <shivamsingh201617@gmail.com>
2023-04-08 15:36:55 +0530

    message

---
+++ index.js
@@ -0,0 +1,7 @@
+
+var button=document.getElementById('btn')
+undefined
+button.addEventListener('click',function(){
+    alert('Submit');
+});
+console.log(hello);
---
+++ swap.js
@@ -0,0 +1,7 @@
+var a=prompt("Enter the value of a");
+var b=Prompt("Enter the value of b");
+var c;
+c=a;
+a=b;
+b=c;
+console.log(a+""+b);
