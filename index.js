acfaa80f0dfdeef9c5ab057242b71b876b8579a5
= <shivamsingh201617@gmail.com>
2023-04-09 01:53:15 +0530

    save the change2

--- index.js
+++ index.js
@@ -15,15 +15,25 @@ item[0].style.fontWeight='bold';
 var item=document.getElementsByClassName("list-group-item")
 
 item[1].style.fontWeight='bold';
+//var item=document.getElementsByClassName("list-group-item")
+
+//item[2].style.fontWeight='bold';
 var item=document.getElementsByClassName("list-group-item")
 
-item[2].style.fontWeight='bold';
-var item=document.getElementsByClassName("list-group-item")
-
-item[3].style.fontWeight='bold';
+ item[3].style.fontWeight='bold';
 
 var items=document.getElementsByClassName("list-group-item")
 undefined
 items[2].style.backgroundColor='green'
-var item=document.getElementsByClassName("group-item")
-item[0].style.fontWeight='bold'
+// var item=document.getElementsByClassName("group-item")
+// item[0].style.fontWeight='bold'
+var secondItem=document.querySelector(".list-group-item:nth-child(2)")
+undefined
+secondItem.style.backgroundColor='green'
+var thirdItem=document.querySelector(".list-group-item:nth-child(3)")
+thirdItem.style.fontWeight='light'
+
+var odd=document.querySelectorAll('li:nth-child(odd)')
+for(var i=0;i<odd.length;i++){
+    odd[i].style.backgroundColor='green';
+}
