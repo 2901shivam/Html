9e611f0f2e3d2bc6b71c4088749fc032f150921f
= <shivamsingh201617@gmail.com>
2023-04-08 17:40:11 +0530

    save the change

--- index.js
+++ index.js
@@ -4,4 +4,25 @@ undefined
 button.addEventListener('click',function(){
     alert('Submit');
 });
-console.log(hello);
+var header=document.getElementById("main-header")
+header.style.border='solid 2px black';
+
+var item=document.getElementsByClassName("list-group-item")
+
+item[0].style.fontWeight='bold';
+
+
+var item=document.getElementsByClassName("list-group-item")
+
+item[1].style.fontWeight='bold';
+var item=document.getElementsByClassName("list-group-item")
+
+item[2].style.fontWeight='bold';
+var item=document.getElementsByClassName("list-group-item")
+
+item[3].style.fontWeight='bold';
+
+var items=document.getElementsByClassName("list-group-item")
+undefined
+items[2].style.backgroundColor='green'
+'green'
