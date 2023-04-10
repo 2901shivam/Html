8dc1cb5d0e5ad1ddc405fd101ee1aa874f2f9742
= <shivamsingh201617@gmail.com>
2023-04-11 03:33:06 +0530

    new change in registration page

--- apponitment.js
+++ apponitment.js
@@ -2,16 +2,24 @@ var take_name=document.getElementById("name");
 var email=document.getElementById("email");
 var phone=document.getElementById("phone");
 var button=document.getElementById('btn')
+// var list=document.getElementById('add-list')
+
 button.addEventListener('click',function(){
-//    localStorage.setItem("name",take_name.value);
-//    localStorage.setItem("email",email.value);
-//    localStorage.setItem("phone",phone.value);
-  let myobject={
+ let myobject={
      name:take_name.value,
      email:email.value,
      phone:phone.value,
   };
   let myobject_serialize=JSON.stringify(myobject);
   localStorage.setItem('myobject',myobject_serialize);
+
+});
+
+button.addEventListener('click',function(){
+    var list=document.getElementById('add-list');
+    var childElement=document.createElement('li');
+    childElement.textContent=take_name.value+'-'+email.value+'-'+phone.value
+    
+    list.appendChild(childElement);
 
 });
