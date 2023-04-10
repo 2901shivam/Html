23a4f28f7862b14a2bd028b33ff4c083bdcb2465
= <shivamsingh201617@gmail.com>
2023-04-11 01:34:21 +0530

    new change

--- apponitment.js
+++ apponitment.js
@@ -3,8 +3,15 @@ var email=document.getElementById("email");
 var phone=document.getElementById("phone");
 var button=document.getElementById('btn')
 button.addEventListener('click',function(){
-   localStorage.setItem("name",take_name.value);
-   localStorage.setItem("email",email.value);
-   localStorage.setItem("phone",phone.value);
+//    localStorage.setItem("name",take_name.value);
+//    localStorage.setItem("email",email.value);
+//    localStorage.setItem("phone",phone.value);
+  let myobject={
+     name:take_name.value,
+     email:email.value,
+     phone:phone.value,
+  };
+  let myobject_serialize=JSON.stringify(myobject);
+  localStorage.setItem('myobject',myobject_serialize);
 
 });
