2d2d3a910070b2d48f02d392719d033414123f2b
= <shivamsingh201617@gmail.com>
2023-04-28 23:58:14 +0530

    SavethedataByusingPostMan

--- apponitment.js
+++ apponitment.js
@@ -1,45 +1,59 @@
-var take_name=document.getElementById("name");
-var email=document.getElementById("email");
-var phone=document.getElementById("phone");
-var button=document.getElementById('btn')
-// var list=document.getElementById('add-list')
-
-button.addEventListener('click',function(){
- let myobject={
-     name:take_name.value,
-     email:email.value,
-     phone:phone.value,
-  };
-  let myobject_serialize=JSON.stringify(myobject);
-  localStorage.setItem('myobject',myobject_serialize);
-
-});
-
-button.addEventListener('click',function(){
-    var list=document.getElementById('add-list');
-    var childElement=document.createElement('li');
-    childElement.textContent=take_name.value+'-'+email.value+'-'+phone.value
-    var deleteButton=document.createElement('input');
-    deleteButton.type='button';
-    deleteButton.value='Delete';
-    deleteButton.addEventListener('click',function(){
-        list.removeChild(childElement);
-    });
-    var editButton=document.createElement('input');
-    editButton.type='button';
-    editButton.value='Edit';
-    editButton.addEventListener('click',function(){
-        //localStorage.removeItem(myobject.email);
-        list.removeChild(childElement);
-        take_name= myobject.name;
-        email=myobject.emial;
-        phone=myobject.phone;
-       
-       
-
-    });
-    childElement.appendChild(deleteButton);
-    childElement.appendChild(editButton);
-     list.appendChild(childElement);
-
-});
+function saveToLocalstorage(event){
+    event.preventDefault();
+    const name=event.target.name.value;
+    const email=event.target.email.value;
+    const phoneNumber=event.target.number.value;
+    //const key = new Date().getTime();
+ 
+   
+    const myObject={
+     name:name,
+     email:email,
+     phoneNumber:phoneNumber,
+    // key,
+ 
+    }
+    // localStorage.setItem(email,JSON.stringify(myObject));
+    axios.post("https://crudcrud.com/api/119d6f851ce741f496d940a053c64ee1/appointmentData",myObject)
+    .then((response)=>{
+        showExpenseOntheScreen(response.data);
+       console.log(response);
+    })
+    .catch((err)=>{
+        console.log(err);
+    })
+        
+    
+    // showExpenseOntheScreen(myObject);
+   }
+    function showExpenseOntheScreen(myObject){
+     const parentElement=document.getElementById("list");
+     const childElement=document.createElement('li');
+    
+     childElement.textContent=myObject.name+'-'+myObject.email+'-'+myObject.phoneNumber;  
+     parentElement.appendChild(childElement);
+     
+     const deleteButton=document.createElement('input');
+     deleteButton.type='button';
+     deleteButton.value='Delete';
+     deleteButton.addEventListener('click',function(){
+         localStorage.removeItem(myObject.email);
+         parentElement.removeChild(childElement);
+     });
+     const editButton=document.createElement('input');
+     editButton.type='button';
+     editButton.value='Edit';
+     editButton.addEventListener('click',function(){
+       localStorage.removeItem(myObject.email);
+       parentElement.removeChild(childElement);
+       document.getElementById("am").value=myObject.name;
+       document.getElementById("dm").value=myObject.email;
+       document.getElementById("ca").value=myObject.phoneNumber;
+ 
+     })
+ 
+     childElement.appendChild(deleteButton);
+     childElement.appendChild(editButton);
+     parentElement.appendChild(childElement);
+ 
+   }
